12f9eff1f5a10e07ac64949327c50212c084a51e
Almaz <atunstyle@gmail.com>
2020-11-03 22:48:29 +0300

    :tada:init commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+
+    return (
+        <div className="App">
+          <h3>Hello DND</h3>
+        </div>
+    );
+}
+
+export default App;
