2f455690222d1303faab86a334c24d3bb39549c9
Almazatun <atunstyle@gmail.com>
2021-09-25 21:45:07 +0300

    Simple refactor

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import style from './App.module.scss';
+import {DragAndDropContainer} from "../compoents/DragAndDrop/DragAndDropContainer";
+
+
+export const App: React.FC = () => {
+    return (
+        <div className={style.App}>
+           <DragAndDropContainer />
+        </div>
+    );
+}
+
