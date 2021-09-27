d3983ada7ec69663c437890c034f6bb98effe29b
Almazatun <atunstyle@gmail.com>
2021-09-27 10:37:54 +0300

    Rename folder

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import style from './App.module.scss';
-import {DragAndDropContainer} from "../compoents/DragAndDrop/DragAndDropContainer";
+import {DragAndDropContainer} from "../components/DragAndDrop/DragAndDropContainer";
 
 
 export const App: React.FC = () => {
